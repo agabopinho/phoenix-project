65454a7baed4859fe012d0c81d07fb425952b64f
Ágabo Pinho <agabo.opah@dotz.com>
2022-06-30 16:01:10 -0300

    chore: refac

---
+++ market_analyzer/Application/Options/OperationSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Options
+{
+    public class OperationSettings
+    {
+        public string? Symbol { get; set; }
+        public DateOnly Date { get; set; }
+        public int ChunkSize { get; set; }
+        public TimeSpan Timeframe { get; set; }
+        public TimeSpan Window { get; set; }
+        public int Deviation { get; set; }
+        public long Magic { get; set; }
+        public bool ExecOrder { get; set; }
+    }
+}
---
+++ market_analyzer/Application/Options/OperationSettingsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Options
+{
+    public static class OperationSettingsExtensions
+    {
+        public static void AddOperationSettings(this IServiceCollection services, Action<OperationSettings> configure)
+        {
+            services.AddOptions<OperationSettings>()
+                .Configure(configure);
+        }
+    }
+}
